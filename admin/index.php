bbb7e42fccac0d3df209a3610494ab79736c4d09
saimonemon46 <saimonemon46@gmail.com>
2026-02-24 02:19:45 +0600

    dashboard

--- admin/index.php
+++ admin/index.php
@@ -26,7 +26,7 @@
 											<input type="text" id="form-field-1" placeholder="Username" class="col-xs-10 col-sm-5" />
 										</div>
 									</div>
-                                    									<div class="form-group">
+                                    <div class="form-group">
 										<label class="col-sm-3 control-label no-padding-right" for="form-field-1"> Text Field </label>
 
 										<div class="col-sm-9">
