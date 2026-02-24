373f4a65408211c8eabbb76f4a6fcac3e5eaebdd
saimonemon46 <saimonemon46@gmail.com>
2026-02-25 00:58:09 +0600

    dashboard and login

---
+++ admin/dashboard.php
@@ -0,0 +1,16 @@
+<?php include 'includes/header.php';?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h2>Apartment Management System</h2>
+        </div>
+    </div>
+
+
+						
+    <?php include 'includes/footer.php'; ?>                                                    
+</div>
+
+
+
