ca9652bf56df8e04c2cdd9c491443f47a7c18a09
saimonemon46 <saimonemon46@gmail.com>
2026-02-25 03:11:15 +0600

    Add visitor and search button

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -67,7 +67,7 @@
 
 						<div class="nav-search" id="nav-search">
 							<form class="form-search">
-								<span class="input-icon">
+								<span class="input-icon" action="#" method="get">
 									<input type="text" placeholder="Search ..." class="nav-search-input" id="nav-search-input" autocomplete="off" />
 									<i class="ace-icon fa fa-search nav-search-icon"></i>
 								</span>
