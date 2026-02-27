09bb0da1207ff5b95a08636d27253d3f2e045ba9
saimonemon46 <saimonemon46@gmail.com>
2026-02-28 03:35:59 +0600

    padding fix

--- admin/view_details.php
+++ admin/view_details.php
@@ -34,88 +34,90 @@ if (!$visitor) {
 // Now include header (after all logic and header calls)
 include 'includes/header.php';
 ?>
-
-<div class="page-header">
-    <h1>
-        Visitor Details
-        <small>
-            <i class="ace-icon fa fa-angle-double-right"></i>
-            Complete visitor information
-        </small>
-    </h1>
-</div>
-
-<?php if (isset($visitor_error) && $visitor_error): ?>
-    <div class="alert alert-warning">
-        <button type="button" class="close" data-dismiss="alert">&times;</button>
-        <strong>Warning!</strong> Visitor not found.
+    <div class="page-header">
+        <h1>
+            Visitor Details
+            <small>
+                <i class="ace-icon fa fa-angle-double-right"></i>
+                Complete visitor information
+            </small>
+        </h1>
     </div>
-    <?php include 'includes/footer.php'; include 'includes/sidebar.php'; ?>
-    </body></html>
-    <?php exit; ?>
-<?php endif; ?>
+<div class="container">
 
-<div class="row">
-    <div class="col-xs-12">
-        <div class="table-responsive">
-            <table class="table table-striped table-hover table-bordered">
-                <tbody>
-                    <tr>
-                        <td><strong>Category</strong></td>
-                        <td><?php echo htmlspecialchars($visitor['category']); ?></td>
-                    </tr>
-                    <tr>
-                        <td><strong>Visitor Name</strong></td>
-                        <td><?php echo htmlspecialchars($visitor['visitor_name']); ?></td>
-                    </tr>
-                    <tr>
-                        <td><strong>Phone</strong></td>
-                        <td><?php echo htmlspecialchars($visitor['phone']); ?></td>
-                    </tr>
-                    <tr>
-                        <td><strong>Apartment No</strong></td>
-                        <td><?php echo htmlspecialchars($visitor['apartment_no']); ?></td>
-                    </tr>
-                    <tr>
-                        <td><strong>Floor No</strong></td>
-                        <td><?php echo htmlspecialchars($visitor['floor']); ?></td>
-                    </tr>
-                    <tr>
-                        <td><strong>Whom to Meet</strong></td>
-                        <td><?php echo htmlspecialchars($visitor['whom_to_meet']); ?></td>
-                    </tr>
 
-                    <tr>
-                        <td><strong>Address</strong></td>
-                        <td><?php echo htmlspecialchars($visitor['address']); ?></td>
-                    </tr>
-                    <tr>
-                        <td><strong>Reason for Visit</strong></td>
-                        <td><?php echo htmlspecialchars($visitor['reason']); ?></td>
-                    </tr>
-                    <tr>
-                        <td><strong>Entry Date</strong></td>
-                        <td><?php echo htmlspecialchars($visitor['entry_time']); ?></td>
-                    </tr>
-                    <tr>
-                        <td><strong>Exit Time</strong></td>
-                        <td>
-                            <?php if (!empty($visitor['exit_time'])): ?>
-                                <?php echo htmlspecialchars($visitor['exit_time']); ?>
-                            <?php else: ?>
-                                <form method="post" class="form-inline" style="display:inline;">
-                                    <input type="hidden" name="mark_out" value="1">
-                                    <button class="btn btn-danger" type="submit">
-                                        <i class="ace-icon fa fa-sign-out bigger-110"></i> Mark Exit
-                                    </button>
-                                </form>
-                            <?php endif; ?>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+    <?php if (isset($visitor_error) && $visitor_error): ?>
+        <div class="alert alert-warning">
+            <button type="button" class="close" data-dismiss="alert">&times;</button>
+            <strong>Warning!</strong> Visitor not found.
         </div>
+        <?php include 'includes/footer.php'; include 'includes/sidebar.php'; ?>
+        </body></html>
+        <?php exit; ?>
+    <?php endif; ?>
 
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="table-responsive">
+                <table class="table table-striped table-hover table-bordered">
+                    <tbody>
+                        <tr>
+                            <td><strong>Category</strong></td>
+                            <td><?php echo htmlspecialchars($visitor['category']); ?></td>
+                        </tr>
+                        <tr>
+                            <td><strong>Visitor Name</strong></td>
+                            <td><?php echo htmlspecialchars($visitor['visitor_name']); ?></td>
+                        </tr>
+                        <tr>
+                            <td><strong>Phone</strong></td>
+                            <td><?php echo htmlspecialchars($visitor['phone']); ?></td>
+                        </tr>
+                        <tr>
+                            <td><strong>Apartment No</strong></td>
+                            <td><?php echo htmlspecialchars($visitor['apartment_no']); ?></td>
+                        </tr>
+                        <tr>
+                            <td><strong>Floor No</strong></td>
+                            <td><?php echo htmlspecialchars($visitor['floor']); ?></td>
+                        </tr>
+                        <tr>
+                            <td><strong>Whom to Meet</strong></td>
+                            <td><?php echo htmlspecialchars($visitor['whom_to_meet']); ?></td>
+                        </tr>
+
+                        <tr>
+                            <td><strong>Address</strong></td>
+                            <td><?php echo htmlspecialchars($visitor['address']); ?></td>
+                        </tr>
+                        <tr>
+                            <td><strong>Reason for Visit</strong></td>
+                            <td><?php echo htmlspecialchars($visitor['reason']); ?></td>
+                        </tr>
+                        <tr>
+                            <td><strong>Entry Date</strong></td>
+                            <td><?php echo htmlspecialchars($visitor['entry_time']); ?></td>
+                        </tr>
+                        <tr>
+                            <td><strong>Exit Time</strong></td>
+                            <td>
+                                <?php if (!empty($visitor['exit_time'])): ?>
+                                    <?php echo htmlspecialchars($visitor['exit_time']); ?>
+                                <?php else: ?>
+                                    <form method="post" class="form-inline" style="display:inline;">
+                                        <input type="hidden" name="mark_out" value="1">
+                                        <button class="btn btn-danger" type="submit">
+                                            <i class="ace-icon fa fa-sign-out bigger-110"></i> Mark Exit
+                                        </button>
+                                    </form>
+                                <?php endif; ?>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+        </div>
     </div>
 </div>
 
